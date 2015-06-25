c58ac59d13b2816d77030e13b412af822bfbda67
ychen <chyu1988@gmail.com>
2015-06-24 22:59:40 -0700

    add two more defects

--- demo.cpp
+++ demo.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <memory.h>
 
 using namespace std;
 
@@ -32,14 +33,43 @@ void test(vector<int> &v1, vector<int> &v2)
 	v2.erase(i);
 }
 
+//Demo Case 3
+int forward_null_example1(int *p) 
+{
+    int x = 1;
+    if ( p == NULL )
+    {
+        x = 0;
+    }
+    else
+    {
+        x = *p;
+    }
+    *p = x;   // Defect: p is potentially NULL
+    return 0;
+}
+
+//Demo Case 4
+struct S
+{
+    int x[10];
+    int y[20];
+};
+void member_boundaries()
+{
+    struct S s;
+    // reported if 'strict_member_boundaries' is enabled
+    memset(&s.x[0], 0, sizeof(int)*30); // access s.x[29]
+}
+
 int main()
 {	
 	cout << "my favorite bugs" << endl;
 
-    	cout << example(1,2,3,4) <<endl;
+   	cout << example(1,2,3,4) <<endl;
 
 	vector<int> vec_1(3,1), vec_2(3,2);
-	test(vec_1,vec_2);	
+	test(vec_1,vec_2);
 
 	return 0;
 	
